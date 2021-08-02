335ec4ae53e2ec9429a1d572cc94999f1c361424
vijay.tembugade <vijay.tembugade@mirraw.com>
2021-08-02 17:10:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [price, setPrice] = useState()
+  const [qty, setQty]= useState()
+  const [currentPrice, setCurrentPrice] = useState()
+  const [output, setOutput] = useState()
+
+  function priceHandler(e){
+    // console.log(e.target.value)
+    setPrice(e.target.value)
+  }
+
+  function handleCheck(){
+    var totalPrice = price * qty;
+    var currentTotalPrice = currentPrice * qty;
+
+  
+    var profit = currentTotalPrice-totalPrice;
+    console.log(profit)
+
+    let percetage = (profit/totalPrice)*100;
+    console.log(percetage)
+
+    if(profit > 0){
+      setOutput(<h2>Congrates ! You made {profit} Rs profit. Total gain is {(percetage)} %</h2>)
+    }else{
+      setOutput(<h2>Awww ! You made {Math.abs(profit)} Rs loss. Total loss is {Math.abs(percetage)} %</h2>)
+    }
+  }
+
+  // console.log(price)
+  // console.log(qty, currentPrice)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Perchace Price</h3>
+      <input onChange={priceHandler} type="number"></input><br/>
+      <h3>Quantity of Stock</h3>
+      <input onChange={(e)=>setQty(e.target.value)} type="number"></input><br/>
+      <h3>Current Price </h3>
+      <input onChange={(e)=>setCurrentPrice(e.target.value)} type="number"></input><br/>
+      <button onClick={handleCheck}>Check</button>
+      {output}
     </div>
   );
 }
